ef924e0b0f7c4ba1bd45bd6110555c8c3f0c4fe5
tomchor <tomaschor@gmail.com>
2021-08-09 16:06:07 -0700

    added tests that compile ke and tke

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,29 @@ using Test
 using Oceananigans
 using Oceanostics
 
+function create_model(; kwargs...)
+
+    model = NonhydrostaticModel(grid = RegularRectilinearGrid(size=(2, 2, 2), extent=(1, 1, 1));
+                                kwargs...
+                                )
+    return model
+end
+
+
+function test_velocity_diagnostics(; model_kwargs...)
+    model = create_model(; model_kwargs...)
+    u, v, w = model.velocities
+
+    ke = KineticEnergy(model)
+    compute!(ke)
+    @test all(interior(ke) .== 0)
+
+    tke = TurbulentKineticEnergy(model, U=0, V=0)
+    compute!(tke)
+    @test all(interior(tke) .== 0)
+    return nothing
+end
+
 @testset "Oceanostics" begin
     topo = (Periodic, Periodic, Bounded)
     grid = RegularRectilinearGrid(topology=topo, size=(4, 5, 6), extent=(1, 1, 1))
@@ -17,4 +40,7 @@ using Oceanostics
 
     tke = TurbulentKineticEnergy(model, U=U, V=V)
     @test tke isa KernelComputedField
+
+    test_velocity_diagnostics()
 end
+
