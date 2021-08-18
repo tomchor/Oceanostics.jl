ce0b4a15930c5c61f85a6101231ea597fa88e13e
tomchor <tomaschor@gmail.com>
2021-08-18 13:59:27 -0700

    made coriolis more general and changed buoyancy dissipation rate to KernelFunctionOperation

--- src/FlowDiagnostics.jl
+++ src/FlowDiagnostics.jl
@@ -26,47 +26,50 @@ function RichardsonNumber(model; N²_bg=0, dUdz_bg=0, dVdz_bg=0)
 end
 
 
-function RossbyNumber(model; dUdy_bg=0, dVdx_bg=0)
+function RossbyNumber(model; dUdy_bg=0, dVdx_bg=0, f=nothing)
     u, v, w = model.velocities
-    f₀ = model.coriolis.f
+    if f==nothing
+        f = model.coriolis.f
+    end
 
     dUdy_tot = ∂y(u) + dUdy_bg
     dVdx_tot = ∂x(v) + dVdx_bg
 
-    return (dVdx_tot - dUdy_tot) / f₀
+    return (dVdx_tot - dUdy_tot) / f
 end
 
 
 
 #++++ Potential vorticity
-function potential_vorticity_in_thermal_wind_fff(i, j, k, grid, u, v, b, f₀)
-    i, j, k = @index(Global, NTuple)
+function potential_vorticity_in_thermal_wind_fff(i, j, k, grid, u, v, b, f)
 
     dVdx =  ℑzᵃᵃᶠ(i, j, k, grid, ∂xᶠᵃᵃ, v) # F, F, C → F, F, F
     dUdy =  ℑzᵃᵃᶠ(i, j, k, grid, ∂yᵃᶠᵃ, u) # F, F, C → F, F, F
     dbdz = ℑxyᶠᶠᵃ(i, j, k, grid, ∂zᵃᵃᶠ, b) # C, C, F → F, F, F
 
-    pv_barot = (f₀ + dVdx - dUdy) * dbdz
+    pv_barot = (f + dVdx - dUdy) * dbdz
 
     dUdz = ℑyᵃᶠᵃ(i, j, k, grid, ∂zᵃᵃᶠ, u) # F, C, F → F, F, F
     dVdz = ℑxᶠᵃᵃ(i, j, k, grid, ∂zᵃᵃᶠ, v) # C, F, F → F, F, F
 
-    pv_baroc = -f₀*(dUdz^2 + dVdz^2)
+    pv_baroc = -f*(dUdz^2 + dVdz^2)
 
     return pv_barot + pv_baroc
 end
 
-function ThermalWindPotentialVorticityᶠᶠᶠ(model)
+function ThermalWindPotentialVorticityᶠᶠᶠ(model; f=nothing)
     u, v, w = model.velocities
     b = model.tracers.b
-    f₀ = model.coriolis.f
+    if f==nothing
+        f = model.coriolis.f
+    end
     return KernelFunctionOperation{Face, Face, Face}(potential_vorticity_in_thermal_wind_fff, model.grid;
-                                                     computed_dependencies=(u, v, w, b), parameters=f₀)
+                                                     computed_dependencies=(u, v, w, b), parameters=f)
 end
 
 
 
-function ertel_potential_vorticity_fff(i, j, k, grid, u, v, w, b, f₀)
+function ertel_potential_vorticity_fff(i, j, k, grid, u, v, w, b, f)
 
     dWdy =  ℑxᶠᵃᵃ(i, j, k, grid, ∂yᵃᶠᵃ, w) # C, C, F  → C, F, F  → F, F, F
     dVdz =  ℑxᶠᵃᵃ(i, j, k, grid, ∂zᵃᵃᶠ, v) # C, F, C  → C, F, F  → F, F, F
@@ -81,35 +84,37 @@ function ertel_potential_vorticity_fff(i, j, k, grid, u, v, w, b, f₀)
     dVdx =  ℑzᵃᵃᶠ(i, j, k, grid, ∂xᶠᵃᵃ, v) # C, F, C  → F, F, C → F, F, F
     dUdy =  ℑzᵃᵃᶠ(i, j, k, grid, ∂yᵃᶠᵃ, u) # F, C, C  → F, F, C → F, F, F
     dbdz = ℑxyᶠᶠᵃ(i, j, k, grid, ∂zᵃᵃᶠ, b) # C, C, C  → C, C, F → F, F, F
-    pv_z = (f₀ + dVdx - dUdy) * dbdz
+    pv_z = (f + dVdx - dUdy) * dbdz
 
     return pv_x + pv_y + pv_z
 end
 
-function ErtelPotentialVorticityᶠᶠᶠ(model)
+function ErtelPotentialVorticityᶠᶠᶠ(model; f=nothing)
     u, v, w = model.velocities
     b = model.tracers.b
-    f₀ = model.coriolis.f
+    if f==nothing
+        f = model.coriolis.f
+    end
     return KernelFunctionOperation{Face, Face, Face}(ertel_potential_vorticity_fff, model.grid;
-                                                     computed_dependencies=(u, v, w, b), parameters=f₀)
+                                                     computed_dependencies=(u, v, w, b), parameters=f)
 end
 #----
 
 
 
 #+++++ Mixing of buoyancy
-@kernel function isotropic_buoyancy_mixing_rate_ccc!(mixing_rate, grid, b, κᵇ, N²₀)
-    i, j, k = @index(Global, NTuple)
+function isotropic_buoyancy_mixing_rate_ccc(i, j, k, grid, b, κᵇ, N²₀)
+
     dbdx² = ℑxᶜᵃᵃ(i, j, k, grid, fψ², ∂xᶠᵃᵃ, b) # C, C, C  → F, C, C  → C, C, C
     dbdy² = ℑyᵃᶜᵃ(i, j, k, grid, fψ², ∂yᵃᶠᵃ, b) # C, C, C  → C, F, C  → C, C, C
     dbdz² = ℑzᵃᵃᶜ(i, j, k, grid, fψ², ∂zᵃᵃᶠ, b) # C, C, C  → C, C, F  → C, C, C
 
-    @inbounds mixing_rate[i, j, k] = κᵇ[i,j,k] * (dbdx² + dbdy² + dbdz²) / N²₀
+    return κᵇ[i,j,k] * (dbdx² + dbdy² + dbdz²) / N²₀
 end
 
 function IsotropicBuoyancyMixingRate(model, b, κᵇ, N²₀; location = (Center, Center, Center), kwargs...)
     if location == (Center, Center, Center)
-        return KernelComputedField(Center, Center, Center, isotropic_buoyancy_mixing_rate_ccc!, model;
+        return KernelFunctionOperation{Center, Center, Center}(isotropic_buoyancy_mixing_rate_ccc, model.grid;
                                    computed_dependencies=(b, κᵇ), parameters=N²₀, kwargs...)
     else
         error("IsotropicBuoyancyMixingRate only supports location = (Center, Center, Center) for now.")
@@ -117,18 +122,18 @@ function IsotropicBuoyancyMixingRate(model, b, κᵇ, N²₀; location = (Center
 end
 
 
-@kernel function anisotropic_buoyancy_mixing_rate_ccc!(mixing_rate, grid, b, params)
-    i, j, k = @index(Global, NTuple)
+function anisotropic_buoyancy_mixing_rate_ccc(mixing_rate, grid, b, params)
+
     dbdx² = ℑxᶜᵃᵃ(i, j, k, grid, fψ², ∂xᶠᵃᵃ, b) # C, C, C  → F, C, C  → C, C, C
     dbdy² = ℑyᵃᶜᵃ(i, j, k, grid, fψ², ∂yᵃᶠᵃ, b) # C, C, C  → C, F, C  → C, C, C
     dbdz² = ℑzᵃᵃᶜ(i, j, k, grid, fψ², ∂zᵃᵃᶠ, b) # C, C, C  → C, C, F  → C, C, C
 
-    @inbounds mixing_rate[i, j, k] = (params.κx*dbdx² + params.κy*dbdy² + params.κz*dbdz²)/params.N²₀
+    return (params.κx*dbdx² + params.κy*dbdy² + params.κz*dbdz²)/params.N²₀
 end
 
 function AnisotropicBuoyancyMixingRate(model, b, κx, κy, κz, N²₀; location = (Center, Center, Center), kwargs...)
     if location == (Center, Center, Center)
-        return KernelComputedField(Center, Center, Center, anisotropic_buoyancy_mixing_rate_ccc!, model;
+        return KernelFunctionOperation{Center, Center, Center}(anisotropic_buoyancy_mixing_rate_ccc, model.grid;
                                    computed_dependencies=(b,),
                                    parameters=(κx=κx, κy=κy, κz=κz, N²₀=N²₀), kwargs...)
     else
