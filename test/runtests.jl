90352db1b28ea40569d4ed5044500e06f2db8f88
tomchor <tomaschor@gmail.com>
2021-08-18 07:35:40 -0700

    separated tests in main runtests.jl script

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,35 @@ using Test
 using Oceananigans
 using Oceanostics
 
-@testset "Oceanostics" begin
-    topo = (Periodic, Periodic, Bounded)
-    grid = RegularRectilinearGrid(topology=topo, size=(4, 5, 6), extent=(1, 1, 1))
-    model = NonhydrostaticModel(grid=grid)
 
+function create_model(; kwargs...)
+    model = NonhydrostaticModel(grid = RegularRectilinearGrid(size=(4, 4, 4), extent=(1, 1, 1));
+                                kwargs...
+                                )
+    return model
+end
+
+function test_velocity_diagnostics(; model_kwargs...)
+    model = create_model(; model_kwargs...)
     u, v, w = model.velocities
-
     U = AveragedField(u, dims=(1, 2))
-    V = AveragedField(u, dims=(1, 2))
+    V = AveragedField(v, dims=(1, 2))
+    W = AveragedField(w, dims=(1, 2))
 
     ke = KineticEnergy(model)
     @test ke isa KernelComputedField
 
-    tke = TurbulentKineticEnergy(model, U=U, V=V)
+    tke = TurbulentKineticEnergy(model, U=U, V=V, W=W)
     @test tke isa KernelComputedField
+
+    return nothing
 end
+
+
+@testset "Oceanostics" begin
+    model = create_model()
+
+    test_velocity_diagnostics()
+end
+
 
