4c1f4bed43b0df43f9b3ac75460d3928e8acead3
tomchor <tomaschor@gmail.com>
2022-02-04 17:50:09 -0800

    fixed isotropic dissipation rates

--- src/TKEBudgetTerms.jl
+++ src/TKEBudgetTerms.jl
@@ -57,26 +57,26 @@ KineticEnergy(model; kwargs...) = KineticEnergy(model, model.velocities...; kwar
 #++++ Energy dissipation rate for a fluid with isotropic viscosity
 function isotropic_viscous_dissipation_rate_les_ccc(i, j, k, grid, u, v, w, ν)
 
-    Σˣˣ² = ∂xᶜᵃᵃ(i, j, k, grid, u)^2
-    Σʸʸ² = ∂yᵃᶜᵃ(i, j, k, grid, v)^2
-    Σᶻᶻ² = ∂zᵃᵃᶜ(i, j, k, grid, w)^2
+    Σˣˣ² = ∂xᶜᶜᶜ(i, j, k, grid, u)^2
+    Σʸʸ² = ∂yᶜᶜᶜ(i, j, k, grid, v)^2
+    Σᶻᶻ² = ∂zᵃᶜᶜ(i, j, k, grid, w)^2
 
-    Σˣʸ² = ℑxyᶜᶜᵃ(i, j, k, grid, fψ_plus_gφ², ∂yᵃᶠᵃ, u, ∂xᶠᵃᵃ, v) / 4
-    Σˣᶻ² = ℑxzᶜᵃᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᵃᵃᶠ, u, ∂xᶠᵃᵃ, w) / 4
-    Σʸᶻ² = ℑyzᵃᶜᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᵃᵃᶠ, v, ∂yᵃᶠᵃ, w) / 4
+    Σˣʸ² = ℑxyᶜᶜᵃ(i, j, k, grid, fψ_plus_gφ², ∂yᶠᶠᶜ, u, ∂xᶠᶠᶜ, v) / 4
+    Σˣᶻ² = ℑxzᶜᵃᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶠᶜᶠ, u, ∂xᶠᶜᶠ, w) / 4
+    Σʸᶻ² = ℑyzᵃᶜᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶜᶠᶠ, v, ∂yᶜᶠᶠ, w) / 4
 
     return ν[i, j, k] * 2 * (Σˣˣ² + Σʸʸ² + Σᶻᶻ² + 2 * (Σˣʸ² + Σˣᶻ² + Σʸᶻ²))
 end
 
 function isotropic_viscous_dissipation_rate_dns_ccc(i, j, k, grid, u, v, w, ν)
 
-    Σˣˣ² = ∂xᶜᵃᵃ(i, j, k, grid, u)^2
-    Σʸʸ² = ∂yᵃᶜᵃ(i, j, k, grid, v)^2
-    Σᶻᶻ² = ∂zᵃᵃᶜ(i, j, k, grid, w)^2
+    Σˣˣ² = ∂xᶜᶜᶜ(i, j, k, grid, u)^2
+    Σʸʸ² = ∂yᶜᶜᶜ(i, j, k, grid, v)^2
+    Σᶻᶻ² = ∂zᵃᶜᶜ(i, j, k, grid, w)^2
 
-    Σˣʸ² = ℑxyᶜᶜᵃ(i, j, k, grid, fψ_plus_gφ², ∂yᵃᶠᵃ, u, ∂xᶠᵃᵃ, v) / 4
-    Σˣᶻ² = ℑxzᶜᵃᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᵃᵃᶠ, u, ∂xᶠᵃᵃ, w) / 4
-    Σʸᶻ² = ℑyzᵃᶜᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᵃᵃᶠ, v, ∂yᵃᶠᵃ, w) / 4
+    Σˣʸ² = ℑxyᶜᶜᵃ(i, j, k, grid, fψ_plus_gφ², ∂yᶠᶠᶜ, u, ∂xᶠᶠᶜ, v) / 4
+    Σˣᶻ² = ℑxzᶜᵃᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶠᶜᶠ, u, ∂xᶠᶜᶠ, w) / 4
+    Σʸᶻ² = ℑyzᵃᶜᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶜᶠᶠ, v, ∂yᶜᶠᶠ, w) / 4
 
     return ν * 2 * (Σˣˣ² + Σʸʸ² + Σᶻᶻ² + 2 * (Σˣʸ² + Σˣᶻ² + Σʸᶻ²))
 end
@@ -119,7 +119,6 @@ function IsotropicViscousDissipationRate(model; U=nothing, V=nothing, W=nothing,
                                                                computed_dependencies=(u, v, w), parameters=ν)
     end
 
-
 end
 
 
@@ -161,7 +160,8 @@ function IsotropicPseudoViscousDissipationRate(model; U=nothing, V=nothing, W=no
     if model.closure isa Oceananigans.TurbulenceClosures.AbstractEddyViscosityClosure
         νₑ = model.diffusivity_fields.νₑ
         return KernelFunctionOperation{Center, Center, Center}(isotropic_pseudo_viscous_dissipation_rate_les_ccc, model.grid;
-                                       computed_dependencies=(u, v, w, νₑ))
+                                                               computed_dependencies=(u, v, w, νₑ))
+
     elseif model.closure == nothing
         error("Trying to calculate a TKE pseudo viscous dissipation rate with `model.closure==nothing`.")
 
