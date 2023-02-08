0a45201a84cef35cd599915b9ff5505ebfcf1384
tomchor <tomaschor@gmail.com>
2023-02-08 10:00:48 -0800

    bit of cleanup

--- src/Oceanostics.jl
+++ src/Oceanostics.jl
@@ -28,16 +28,16 @@ using Oceananigans.TurbulenceClosures: νᶜᶜᶜ, calc_nonlinear_κᶜᶜᶜ
 
 # End point
 @inline _νᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple{}, K::Tuple{}, clock) = zero(eltype(grid))
-@inline _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple{}, c, id, velocities) = zero(eltype(grid))
+@inline _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple{}, args...) = zero(eltype(grid))
 
 # "Inner-outer" form (hopefully) makes the compiler "unroll" the loop over a tuple:
 @inline _νᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple, K::Tuple, clock) =
      νᶜᶜᶜ(i, j, k, grid, closure_tuple[1],     K[1],     clock) + 
     _νᶜᶜᶜ(i, j, k, grid, closure_tuple[2:end], K[2:end], clock)
 
-@inline _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple, c, id, velocities) = 
-    calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple[1], c, id, velocities) + 
-    _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple[2:end], c, id, velocities)
+@inline _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple::Tuple, args...) =
+    calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple[1], args...) +
+    _calc_nonlinear_κᶜᶜᶜ(i, j, k, grid, closure_tuple[2:end], args...)
 
 
 include("TKEBudgetTerms.jl")
