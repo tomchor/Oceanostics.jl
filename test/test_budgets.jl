c6329bfd644f4055eed33fa54e331ff9c59ae24a
tomchor <tomaschor@gmail.com>
2023-03-15 12:20:30 -0700

    better criterion for testing tracer variance

--- test/test_budgets.jl
+++ test/test_budgets.jl
@@ -1,8 +1,21 @@
 using Oceananigans.TurbulenceClosures: HorizontalFormulation, VerticalFormulation
 using Oceananigans.Fields: @compute
+using Random
+using Statistics
 
-function test_tracer_variance_budget(; N=4, κ=2, rtol=0.01)
-    closure = ScalarDiffusivity(HorizontalFormulation(), κ=κ)
+function periodic_locations(N_locations, flip_z=true)
+    Random.seed!(772)
+    reflections = [-2;;-1;;0;;1;;2]
+
+    x₀ =  rand(N_locations) .+ reflections
+    y₀ =  rand(N_locations) .+ reflections
+    z₀ = -rand(N_locations) .+ reflections
+
+    return x₀, y₀, z₀
+end
+
+function test_tracer_variance_budget(; N=16, κ=2, rtol=0.01)
+    closure = ScalarDiffusivity(κ=κ)
 
     grid = RectilinearGrid(topology=(Periodic, Periodic, Periodic),
                            size=(N,N,N), extent=(1,1,1))
@@ -14,18 +27,16 @@ function test_tracer_variance_budget(; N=4, κ=2, rtol=0.01)
     σz = 4grid.Δzᵃᵃᶜ # z length scale of the noise
 
     N_gaussians = 20 # How many Gaussians do we want sprinkled throughout the domain?
-    x₀ = grid.Lx * rand(N_gaussians); y₀ = grid.Ly * rand(N_gaussians); z₀ = -grid.Lz * rand(N_gaussians) # Locations of the Gaussians
-
-    xₚ = x₀ .+ (grid.Lx .* [-2;;-1;;0;;1;;2]) # Make that noise periodic by "infinite" horizontal reflection
-    yₚ = y₀ .+ (grid.Ly .* [-2;;-1;;0;;1;;2]) # Make that noise periodic by "infinite" horizontal reflection
-    zₚ = z₀ .+ (grid.Lz .* [-2;;-1;;0;;1;;2]) # Make that noise periodic by "infinite" horizontal reflection
+    Random.seed!(772)
+    xₚ, yₚ, zₚ = periodic_locations(20)
 
     resolved_noise(x, y, z) = sum(@. exp(-(x-xₚ)^2/σx^2 -(y-yₚ)^2/σy^2 -(z-zₚ)^2/σz^2))
     set!(model, c=resolved_noise)
-
-    simulation = Simulation(model; Δt=grid.Δxᶜᵃᵃ^2/κ/20, stop_time=0.1)
-
     c = model.tracers.c
+    c.data.parent .-= mean(c)
+
+    simulation = Simulation(model; Δt=grid.Δxᶜᵃᵃ^2/κ/50, stop_time=0.1)
+
     χ  = Oceanostics.FlowDiagnostics.TracerVarianceDissipationRate(model, :c)
 
     @compute ∫χdV   = Field(Integral(χ))
@@ -45,10 +56,10 @@ function test_tracer_variance_budget(; N=4, κ=2, rtol=0.01)
     compute!(∫c²dV)
     ∫c²dV_tᶠ = parent(∫c²dV)[1,1,1]
     ∫∫χdVdt_tᶠ = ∫c²dV_t⁰- model.auxiliary_fields.∫∫χdVdt
-    abs_error = (abs(∫∫χdVdt_tᶠ - ∫c²dV_tᶠ)/∫c²dV_tᶠ)
+    abs_error = (abs(∫∫χdVdt_tᶠ - ∫c²dV_tᶠ)/∫c²dV_t⁰)
 
     @info "Error in c² decrease is $abs_error"
-    @test ≈(∫∫χdVdt_tᶠ, ∫c²dV_tᶠ, rtol=rtol)
+    @test abs_error < rtol
 
     return nothing
 end
