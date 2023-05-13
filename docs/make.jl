375d3a7c16ad99b0da784a185d6144729c7a6cea
tomchor <tomaschor@gmail.com>
2023-05-13 15:16:42 -0700

    better make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,16 +13,17 @@ using Oceanostics
 EXAMPLES_DIR = joinpath(@__DIR__, "examples")
 OUTPUT_DIR   = joinpath(@__DIR__, "src/generated")
 
-examples = ["two_dimensional_turbulence.jl",
-           ]
+examples = ["Two-dimensional turbulence"   => "two_dimensional_turbulence",
+            "Kelvin-Helmholtz instability" => "kelvin_helmholtz",
+            ]
 
-for example in examples
+example_codes = [ v * ".jl" for (k, v) in examples ]
+example_pages = [ k => "generated/$v.md" for (k, v) in examples ]
+
+for example in example_codes
     example_filepath = joinpath(EXAMPLES_DIR, example)
     Literate.markdown(example_filepath, OUTPUT_DIR; flavor = Literate.DocumenterFlavor())
 end
-
-example_pages = ["Two-dimensional turbulence" => "generated/two_dimensional_turbulence.md",
-                 ]
 #---
 
 
