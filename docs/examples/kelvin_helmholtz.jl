3e02af2fbf8d1e4266250756ae8180fa2db2eee7
tomchor <tomaschor@gmail.com>
2023-08-20 13:02:00 -0700

    fix kelvin helmholtz example

--- docs/examples/kelvin_helmholtz.jl
+++ docs/examples/kelvin_helmholtz.jl
@@ -121,15 +121,15 @@ ax3 = Axis(fig[2, 3]; title = "b", kwargs...);
 
 n = Observable(1)
 
-Riₙ = @lift ds.Ri[Ti=$n, yC=Near(0)]
+Riₙ = @lift set(ds.Ri[Ti=$n, yC=Near(0)], :xC => X, :zF => Z)
 hm1 = heatmap!(ax1, Riₙ; colormap = :bwr, colorrange = (-1, +1))
 Colorbar(fig[3, 1], hm1, vertical=false, height=8)
 
-Qₙ = @lift ds.Q[Ti=$n, yC=Near(0)]
+Qₙ = @lift set(ds.Q[Ti=$n, yC=Near(0)], :xC => X, :zC => Z)
 hm2 = heatmap!(ax2, Qₙ; colormap = :inferno, colorrange = (0, 0.2))
 Colorbar(fig[3, 2], hm2, vertical=false, height=8)
 
-bₙ = @lift ds.b[Ti=$n, yC=Near(0)]
+bₙ = @lift set(ds.b[Ti=$n, yC=Near(0)], :xC => X, :zC => Z)
 hm3 = heatmap!(ax3, bₙ; colormap = :balance, colorrange = (-2.5e-2, +2.5e-2))
 Colorbar(fig[3, 3], hm3, vertical=false, height=8);
 
