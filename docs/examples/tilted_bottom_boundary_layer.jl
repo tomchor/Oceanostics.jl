8ecf1108c6617c335c89943cc4a61d4254f3898b
tomchor <tomaschor@gmail.com>
2023-08-20 15:00:14 -0700

    fix text

--- docs/examples/tilted_bottom_boundary_layer.jl
+++ docs/examples/tilted_bottom_boundary_layer.jl
@@ -131,8 +131,8 @@ simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(4))
 
 # ## Model diagnostics
 #
-# We set-up a custom progress messenger using the Oceanostics`TimedMessenger`, which displays, among other
-# information, the time step duration
+# We set-up a custom progress messenger using `Oceanostics.ProgressMessengers`, which allows the
+# user to combine different `ProgressMessenger`s into one:
 
 using Oceanostics.ProgressMessengers
 
