fe0112a190265c4375a89870e64ba838ada050a0
tomaschor <tomaschor@gmail.com>
2025-07-15 14:25:24 -0700

    fix docstring

--- src/KineticEnergyEquation.jl
+++ src/KineticEnergyEquation.jl
@@ -456,7 +456,6 @@ const DissipationRate = KineticEnergyDissipationRate
 
 Calculate the Kinetic Energy Dissipation Rate, defined as
 
-    ε = ν (∂uᵢ/∂xⱼ) (∂uᵢ/∂xⱼ)
     ε = ∂ⱼuᵢ ⋅ Fᵢⱼ
 
 where ∂ⱼuᵢ is the velocity gradient tensor and Fᵢⱼ is the stress tensor.
